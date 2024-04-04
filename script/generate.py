a677d2cd2f0df6a283143e88fbcf876d8b5dc42a
Tim Kretschmer <xbigtk13x@gmail.com>
2024-04-04 17:26:08 -0500

    Disney assets and more automa rules

--- script/generate.py
+++ script/generate.py
@@ -8,7 +8,7 @@ with open('print-and-play/nandeck/disney.csv','r') as read_handle:
     count = 0
     for row in csv_reader:
         if count > 0:
-            disney_villains.append({
+            villain = {
                 'name': row[0],
                 'box': row[1],
                 'special': row[2],
@@ -17,7 +17,10 @@ with open('print-and-play/nandeck/disney.csv','r') as read_handle:
                 'fate2': row[5],
                 'fate3': row[6],
                 'brain_id': f'disney_{count + 1:02d}.jpg'
-            })
+            }
+            if villain['name'] in ['Captain Hook', 'Maleficent', 'Prince John', 'Ursula']:
+                villain['name'] = f'{villain["name"]} ({"ItE" if "Intro" in villain["box"] else "TWTiA"})'
+            disney_villains.append(villain)
         count += 1
 
 marvel_villains = []
@@ -37,13 +40,14 @@ with open('print-and-play/nandeck/marvel.csv','r') as read_handle:
                 'brain_id': f'marvel_{count + 1:02d}.jpg'
             })
         count += 1
-
 villains_content = 'window.villains_list = {'
 villains_content += "\n\tdisney: ["
+disney_villains = sorted(disney_villains, key=lambda x: x['name'])
 for vv in disney_villains:
     villains_content += f'\n\t\t{{name: "{vv["name"]}", box: "{vv["box"]}", special: "{vv["special"]}", preference: "{vv["preference"]}", fate1: "{vv["fate1"]}", fate2: "{vv["fate2"]}", fate3: "{vv["fate3"]}", brainId: "{vv["brain_id"]}"}},'
 villains_content += "\n\t],"
 villains_content += "\n\tmarvel: ["
+marvel_villains = sorted(marvel_villains, key=lambda x: x['name'])
 for vv in marvel_villains:
     villains_content += f'\n\t\t{{name: "{vv["name"]}", box: "{vv["box"]}", special: "{vv["special"]}", preference: "{vv["preference"]}", fate1: "{vv["fate1"]}", fate2: "{vv["fate2"]}", fate3: "{vv["fate3"]}", brainId: "{vv["brain_id"]}"}},'
 villains_content += "\n\t]"
